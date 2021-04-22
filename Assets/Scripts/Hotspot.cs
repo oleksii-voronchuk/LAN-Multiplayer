9c0e8e1dadb97b41eec898c1959e4fc78522be83
Oleksii Voronchuk <56025008+oleksii-voronchuk@users.noreply.github.com>
2021-04-22 23:10:30 +0300

    Added Hotspot for Windows

---
+++ Assets/Scripts/Hotspot.cs
@@ -0,0 +1,21 @@
+﻿using Assets.Scripts.Network;
+using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Hotspot : MonoBehaviour
+    {
+        [SerializeField] private string ssid;
+        [SerializeField] private string password;
+
+        private IEnumerator Start()
+        {
+            NetworkAdapter.StopHotspot();
+            yield return new WaitForSecondsRealtime(5);
+            NetworkAdapter.StartHotspot(ssid, password);
+            yield return new WaitForSecondsRealtime(10);
+            NetworkAdapter.StopHotspot();
+        }
+    }
+}
