4373edd503e6c64f0374c9a54267bf4e8183808a
Oleksii Voronchuk <56025008+oleksii-voronchuk@users.noreply.github.com>
2021-06-14 01:22:13 +0300

    Added WiFi finder

---
+++ Assets/Scripts/Extensions/StringExtensions.cs
@@ -0,0 +1,32 @@
+﻿namespace Assets.Scripts.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string FindSSID(this string str)
+        {
+            var strings = str.Split('\n');
+            foreach (var s in strings)
+            {
+                if (s.Contains("SSID"))
+                {
+                    return s.Replace("SSID", "").Replace(":", "").Trim(' ');
+                }
+            }
+
+            return string.Empty;
+        }
+
+        public static string FindSSIDKey(this string str)
+        {
+            var strings = str.Split('\n');
+            foreach (var s in strings)
+            {
+                if (s.Contains("Content"))
+                {
+                    return s.Replace("Key Content", "").Replace(":", "").Trim(' ');
+                }
+            }
+            return string.Empty;
+        }
+    }
+}
--- Assets/Scripts/Network/NetworkAdapter.cs
+++ Assets/Scripts/Network/NetworkAdapter.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Assets.Scripts.Extensions;
+using System;
 using System.Diagnostics;
 using System.Linq;
 
@@ -6,17 +7,16 @@ namespace Assets.Scripts.Network
 {
     public static class NetworkAdapter
     {
-
-        public static string StartHotspot(string ssid, string password)
+        public static string StartHotspot(WLANProfile wlanProfile)
         {
             StopHotspot();
             var addressees = GetIPs();
             var commandProcess = StartCommandProcess();
-            commandProcess.StandardInput.WriteLine($"netsh wlan set hostednetwork mode=allow ssid={ssid} key={password}");
+            commandProcess.StandardInput.WriteLine($"netsh wlan set hostednetwork mode=allow ssid={wlanProfile.SSID} key={wlanProfile.Key}");
             commandProcess.StandardInput.WriteLine("netsh wlan start hostednetwork");
             StopCommandProcess(commandProcess);
             var newAddressees = GetIPs();
-            return newAddressees.Except(addressees).LastOrDefault();
+            return newAddressees.Except(addressees).LastOrDefault()?.Trim(' ');
         }
 
         public static void StopHotspot()
@@ -34,7 +34,8 @@ namespace Assets.Scripts.Network
                 FileName = "cmd.exe",
                 RedirectStandardInput = true,
                 RedirectStandardOutput = true,
-                UseShellExecute = false
+                UseShellExecute = false,
+                CreateNoWindow = true
             };
 
             return Process.Start(processStartInfo);
@@ -54,6 +55,37 @@ namespace Assets.Scripts.Network
             var addressees = commandProcess.StandardOutput.ReadToEnd().Replace("IP Address:", string.Empty);
             StopCommandProcess(commandProcess);
             return addressees.Split('\n');
+        }
+
+        public static WLANProfile GetProfile()
+        {
+            var profile = new WLANProfile();
+            var commandProcess = StartCommandProcess();
+            commandProcess.StandardInput.WriteLine("cls");
+            commandProcess.StandardInput.WriteLine("netsh wlan show interfaces");
+            commandProcess.StandardInput.Close();
+            profile.SSID = commandProcess.StandardOutput.ReadToEnd().FindSSID();
+            StopCommandProcess(commandProcess);
+
+            var keyCommandProcess = StartCommandProcess();
+            keyCommandProcess.StandardInput.WriteLine($"netsh wlan show profile {profile.SSID} key=clear");
+            keyCommandProcess.StandardInput.Close();
+            profile.Key = keyCommandProcess.StandardOutput.ReadToEnd().FindSSIDKey();
+            StopCommandProcess(keyCommandProcess);
+
+            return profile;
+        }
+    }
+
+    public struct WLANProfile
+    {
+        public string SSID;
+        public string Key;
+
+        public WLANProfile(string ssid, string key)
+        {
+            SSID = ssid;
+            Key = key;
         }
     }
 }
